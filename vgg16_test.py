cb9d661efc8feebdd4d1c54f8dd26b7f9c6458df
bbiyak2929 <hungryshark4292@gmail.com>
2024-04-11 20:05:51 +0900

    test

--- vgg16_test.py
+++ vgg16_test.py
@@ -1,13 +1,42 @@
+from keras.models import load_model
 import cv2
+import numpy as np
 
 cap = cv2.VideoCapture(0)
+size = (64, 64)
+model = load_model('/home/modeep3/바탕화면/AI-testv_1/model64.keras')
 
-while True:
+labels_dict = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8, 'J': 9, 'K': 10, 'L': 11, 'M': 12,
+               'N': 13, 'O': 14, 'P': 15, 'Q': 16, 'R': 17, 'S': 18, 'T': 19, 'U': 20, 'V': 21, 'W': 22, 'X': 23, 'Y': 24,
+               'Z': 25, 'space': 26, 'del': 27, 'nothing': 28}
+result_dict = {labels_dict[i]:i for i in labels_dict}
+
+def preprocessing(frame):
+    frame = cv2.resize(frame, size)
+    print(frame.shape)
+    image = np.array([frame])
+    print(image.shape)
+    image = image.astype('float32')/255.0
+    print(image.shape)
+    return image
+
+while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
+        print('카메라 오류 발생')
         break
-    cv2.imshow('cam_test', cv2.flip(frame, 1))
-    if cv2.waitKey(1) == ord('q'):
+    print(frame.shape)
+    cv2.imshow('frame', frame)
+    pre_image = preprocessing(frame)
+    result = model.predict(pre_image).squeeze()
+    idx = int(np.argmax(result))
+    text = result_dict[idx]
+    frame = cv2.resize(frame, dsize=(720,1080), interpolation=cv2.INTER_LINEAR)
+    frame = cv2.flip(frame, 1)
+    cv2.putText(frame, text, org=(540, 320), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=3, color=(255,255,255), thickness=2, lineType=cv2.LINE_AA)
+    cv2.imshow('ASL_Transform', frame)
+    if cv2.waitKey(100) == ord('q'):
+        print('프로그램을 종료합니다.')
         break
 
 cap.release()
--- vgg16_train.py
+++ vgg16_train.py
@@ -3,21 +3,20 @@ import os
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split, GridSearchCV
 import cv2
-import keras
-from keras.layers import Conv2D, MaxPool2D, Flatten, Dense, Dropout, BatchNormalization,Activation,MaxPooling2D
+from keras.layers import Conv2D, MaxPool2D, Flatten, Dense, Dropout, BatchNormalization,Activation,MaxPooling2D, Input
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
-from tensorflow.keras.optimizers import Adam
+from keras.initializers import Orthogonal
+from keras.optimizers import Adam
 from keras import regularizers
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
-from tensorflow.keras.applications import VGG16
+from keras.applications.vgg16 import VGG16
 from keras.models import Model
 import tensorflow as tf
 from keras.callbacks import ReduceLROnPlateau, ModelCheckpoint
-from tensorflow.keras.applications import ResNet50
-from tensorflow.keras.utils import to_categorical
+from keras.utils import to_categorical
 from keras.metrics import AUC
 from keras.metrics import Precision
 from keras.metrics import Recall
@@ -37,11 +36,11 @@ labels_dict = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, '
                'N': 13, 'O': 14, 'P': 15, 'Q': 16, 'R': 17, 'S': 18, 'T': 19, 'U': 20, 'V': 21, 'W': 22, 'X': 23,
                'Y': 24,
                'Z': 25, 'space': 26, 'del': 27, 'nothing': 28}
-
+size = (64, 64)
+init = Orthogonal(gain=1.0, seed=None)
 def load_train_data():
     Y_train = []
     X_train = []
-    size = 64, 64
     print("LOADING DATA FROM : ", end="")
     for folder in os.listdir(train_dir):
         print(folder, end=' | ')
@@ -71,7 +70,6 @@ def load_train_data():
 def load_test_data():
     labels = []
     X_test = []
-    size = 64, 64
     for image in os.listdir(test_dir):
         # read image
         temp_img = cv2.imread(test_dir + '/'+ image,0)
@@ -103,9 +101,6 @@ print("Y_train befor one-hot encoder : ",Y_train[0])
 
 print('Loaded', len(Y_test), 'images for testing,', 'Test data shape =', Y_test.shape)
 
-
-
-
 datagen = ImageDataGenerator(
     featurewise_center=False,  # set input mean to 0 over the dataset
         samplewise_center=False,  # set each sample mean to 0
@@ -120,28 +115,29 @@ datagen = ImageDataGenerator(
         vertical_flip=False )
 datagen.fit(X_train)
 
-
 model = Sequential()
 
-model.add(VGG16(weights='imagenet', include_top=False, input_shape=(64,64,3)))
+model.add(Input(shape=(size[0],size[1],3)))
+
+model.add(VGG16(weights='imagenet', include_top=False))
 
 model.add(Flatten())
 
-model.add(Dense(420, activation='relu')) 
+model.add(Dense(420, activation='relu', kernel_initializer=init))
+model.add(Dropout(0.4))
+model.add(Dense(29, activation='softmax', kernel_initializer=init))
 
-model.add(Dense(29, activation='softmax'))
-
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=[AUC(name = 'Accuracy'),Precision(name = 'Precision'),Recall(name = 'Recall')] )#optimizer = sigmoed
+model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=[AUC(name = 'Accuracy'),Precision(name = 'Precision'),Recall(name = 'Recall')] )#optimizer = sigmoed
 
 model.summary()
 
 tf.config.run_functions_eagerly(True)
 history = model.fit(
-    datagen.flow(X_train,Y_train, batch_size = 64),
-    epochs = 200,
+    datagen.flow(X_train,Y_train, batch_size = 32),
+    epochs = 100,
     validation_data = (X_test, Y_test),
     callbacks = [
-        ModelCheckpoint('model.keras', verbose=1, save_best_only=True),
-        ReduceLROnPlateau(verbose=1, factor=0.5, patience=50, mode='auto')
+        ModelCheckpoint('model64.keras', verbose=1, save_best_only=True),
+        ReduceLROnPlateau(monitor='val_loss', verbose=1, patience=10, mode='auto'),
     ]
 )
